a96d5adc4de538c61a03f007cf0b2fc099f77d66
Claude Ayitey <clauditey@gmail.com>
2018-01-27 20:16:01 +0000

    model ready for adonkra name uploa. Important files renamed to haml now.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   resources :pans
   root 'pages#index'
 
-  get 'process' => 'pages#process'
-  get 'about'   => 'pages#about'
-  get 'help'    => 'pages#help'
-  get 'wiki'    => 'pages#wiki'
+  get 'process'   => 'pages#process'
+  get 'about'     => 'pages#about'
+  get 'help'      => 'pages#help'
+  get 'wiki'      => 'pages#wiki'
+  get 'adinkra'   => 'adinkra_symbols#index'
 
   resources :adinkra_symbols
-  
-  root 'application#hello'
+
 end
