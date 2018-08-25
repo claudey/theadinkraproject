fd9a7aeca9a57df42dca95d0634002969557e174
Claude Ayitey <clauditey@gmail.com>
2018-08-25 11:54:06 +0000

    added a new short_url column to help with url naming

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119000006) do
+ActiveRecord::Schema.define(version: 20180825105902) do
 
   create_table "adinkra_symbols", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180119000006) do
     t.string "theme"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "short_url"
   end
 
 end
